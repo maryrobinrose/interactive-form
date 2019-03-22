78cdfff8ba7c4e5cbca5eb8bf9d5ffc9b2c8fa29
Mary Robin Rose <maryrobinrose@gmail.com>
2019-03-22 14:01:38 -0700

    add other title text field

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+const otherTitle = $('#other-title');
+
+//Set focus on first text field when page loads
+$(document).ready(function() {
+    $('form:first *:input[type!=hidden]:first').focus();
+});
+
+/*”Job Role” section
+
+Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
+Give the field an id of “other-title,” and add the placeholder text of "Your Job Role".
+Note: You'll need to add the "Other" job role input directly into the HTML and hide it initially with JS in order to get this feature to work when JS is disabled, which is a requirement below.*/
+
+//Hides #other-title text field initially
+otherTitle.hide();
+
+//Show #other-title text field when "Other" is clicked
+$('otherTitle').change(function() {
+  if ($(this).val() === 'Other') {
+      $(this).replaceWith('<input name="Your Job Role" type="text" />');
+    }
+});
+
+
 console.log('hi');
