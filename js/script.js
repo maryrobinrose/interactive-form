030a4105c0d8895bd78a30be3b5eaee25ca2aa3f
Mary Robin Rose <maryrobinrose@gmail.com>
2019-03-22 14:09:36 -0700

    add comments

--- js/script.js
+++ js/script.js
@@ -5,16 +5,10 @@ $(document).ready(function() {
     $('form:first *:input[type!=hidden]:first').focus();
 });
 
-/*”Job Role” section
-
-Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
-Give the field an id of “other-title,” and add the placeholder text of "Your Job Role".
-Note: You'll need to add the "Other" job role input directly into the HTML and hide it initially with JS in order to get this feature to work when JS is disabled, which is a requirement below.*/
-
-//Hides #other-title text field initially
+//Hides otherTitle text field when page loads
 $otherTitle.hide();
 
-//Show #other-title text field when "Other" is clicked
+//Show otherTitle text field when "Other" is selected, hide when unselected
 $('#title').change(function() {
   if ($(this).val() === 'other') {
       $otherTitle.show();
