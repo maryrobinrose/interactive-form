2829e68b565318336fd19eb286accd09874871b4
Mary Robin Rose <maryrobinrose@gmail.com>
2019-03-22 19:29:30 -0700

    add color option attr

--- js/script.js
+++ js/script.js
@@ -22,9 +22,8 @@ $('#title').change(function() {
 For the T-Shirt "Color" menu, only display the color options that match the design selected in the "Design" menu.
 If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
 If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
-When a new theme is selected from the "Design" menu, the "Color" field and drop down menu is updated.
+When a new theme is selected from the "Design" menu, the "Color" field and drop down menu is updated.*/
 
-here are two great properties that are helpful for manipulating those option tags.  When you want to choose which option tag shows in the `select` element, you can use the handy setAttribute method to give it the `selected` attribute, and set it to true.  And when you want to hide an option tag, you can give it the `hidden` attribute, set to true.  And then when you want to show that one again, you just use removeAttribute to drop the `hidden` attribute.*/
 
 //Hides color options on page load
 $('#colors-js-puns').hide();
@@ -34,12 +33,30 @@ $('#design').change(function() {
   if (this.value == 'js puns') {
     $('#colors-js-puns').show();
     $('#color option[value="cornflowerblue"]').attr('selected', true).attr('hidden', false);
-  } //else if {
+    $('#color option[value="darkslategrey"]').attr('selected', true).attr('hidden', false);
+    $('#color option[value="gold"]').attr('selected', true).attr('hidden', false);
 
-  //} else {
+    $('#color option[value="tomato"]').attr('selected', false).attr('hidden', true);
+    $('#color option[value="steelblue"]').attr('selected', false).attr('hidden', true);
+    $('#color option[value="dimgrey"]').attr('selected', false).attr('hidden', true);
+  } else if {
+    $('#color option[value="cornflowerblue"]').attr('selected', false).attr('hidden', true);
+    $('#color option[value="darkslategrey"]').attr('selected', false).attr('hidden', true);
+    $('#color option[value="gold"]').attr('selected', false).attr('hidden', true);
 
-  //}
+    $('#color option[value="tomato"]').attr('selected', true).attr('hidden', false);
+    $('#color option[value="steelblue"]').attr('selected', true).attr('hidden', false);
+    $('#color option[value="dimgrey"]').attr('selected', true).attr('hidden', false);
+  } else {
+    $('#colors-js-puns').hide();
+  }
 });
+
+/*”Register for Activities” section
+
+Some events are at the same day and time as others. If the user selects a workshop, don't allow selection of a workshop at the same day and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
+When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.
+As a user selects activities, a running total should display below the list of checkboxes. For example, if the user selects "Main Conference", then Total: $200 should appear. If they add 1 workshop, the total should change to Total: $300.*/
 
 
 
